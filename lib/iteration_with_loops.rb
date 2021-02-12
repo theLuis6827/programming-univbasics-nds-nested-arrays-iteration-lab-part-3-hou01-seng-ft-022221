e36cd4bc5579ccbef01c09e5f46d4b3347f2ca51
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-12 20:58:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,21 +13,3 @@ end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
-
-
-
-
-new_array=[]
- counter1=0
-  while counter1 < src.length do
-   counter2=0 
-    while counter2 < src[counter1].length do 
-      if src[counter1][counter2].class == String
-        phrase= src[counter1][counter2].join(' ') 
-      new_array << phrase  
-      counter2=counter2+1  
-     end  
-    end  
-   counter1=counter1+1  
-  end  
- new_array  
